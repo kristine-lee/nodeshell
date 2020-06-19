15d33ba4c3b57dfc2c3f3b5c92f18478e0941142
Kristine Lee <kristine0105@gmail.com>
2020-06-19 14:52:42 -0400

    pwd and nodemon

--- bash.js
+++ bash.js
@@ -4,11 +4,15 @@ const filePath = module.path // we have acces
 process.stdout.write('prompt > ');
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
-    // if (cmd === "pwd"){
-        
-    // }
-    process.stdout.write("You typed: " + cmd);
-    process.stdout.write('\nprompt > ');
-    
+    console.log("This is our console.log: ", cmd)
+    if (cmd === "pwd"){
+        process.stdout.write(filePath)
+    }
+    else{
+        process.stdout.write("You typed: " + cmd);
+        process.stdout.write('\nprompt > ');
+    }
+
 });
+
 //console.log(module)
